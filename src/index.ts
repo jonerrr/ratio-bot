9fb6fc5e8372aaf16022b2dd41dd0549e74a7f83
jonah <73760377+jonerrr@users.noreply.github.com>
2022-01-31 23:19:15 -0500

    fix leaderboards, switch error logging, add multiple voting emojis, and change how ratios are detected

--- src/index.ts
+++ src/index.ts
@@ -1,248 +1,225 @@
 import {
-  Client,
-  WebhookClient,
-  Intents,
-  Message,
-  MessageEmbed,
-  MessageReaction,
-  PartialMessageReaction,
+    Client,
+    Intents,
+    Message,
+    MessageEmbed,
+    MessageReaction,
+    PartialMessageReaction,
 } from "discord.js";
-import { SlashCommandBuilder } from "@discordjs/builders";
-import { REST } from "@discordjs/rest";
-import { Routes } from "discord-api-types/v9";
-import { PrismaClient } from "@prisma/client";
-import { Api } from "@top-gg/sdk";
-import { config } from "dotenv";
-config();
+import {SlashCommandBuilder} from "@discordjs/builders";
+import {REST} from "@discordjs/rest";
+import * as Sentry from "@sentry/node";
+import {Routes} from "discord-api-types/v9";
+import {PrismaClient} from "@prisma/client";
+import {Api} from "@top-gg/sdk";
+import {config} from "dotenv";
 
+config();
+Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    tracesSampleRate: 0.25,
+});
 const topGG = new Api(process.env.TOPGG_TOKEN);
 const client = new Client({
-  intents: [
-    Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
-    Intents.FLAGS.GUILD_MESSAGES,
-    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-  ],
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    ],
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
 });
-const webhookClient = new WebhookClient({ url: process.env.WEBHOOK_URL });
-const rest = new REST({ version: "9" }).setToken(process.env.TOKEN);
+const rest = new REST({version: "9"}).setToken(process.env.TOKEN);
 const prisma = new PrismaClient();
 
 let emojis: string[] = [
-  ":one:",
-  ":two:",
-  ":three:",
-  ":four:",
-  ":five:",
-  ":six:",
-  ":seven:",
-  ":eight:",
-  ":nine:",
-  ":keycap_ten:",
+    ":one:",
+    ":two:",
+    ":three:",
+    ":four:",
+    ":five:",
+    ":six:",
+    ":seven:",
+    ":eight:",
+    ":nine:",
+    ":keycap_ten:",
 ];
+let votedEmojis: string[] = process.env.VOTED_EMOJIS.split(",")
 const md = "`";
 let cacheExpire: number = Date.now();
 
 client.on("ready", async () => {
-  await prisma.$connect();
-  console.log(`Logged in as ${client.user.tag}\nMode: ${process.env.MODE}`);
+    await prisma.$connect();
+    console.log(`Logged in as ${client.user.tag}\nMode: ${process.env.MODE}`);
 
-  client.user.setPresence({
-    status: "dnd",
-    activities: [{ name: `Ratio Bot ${process.env.MODE}`, type: "PLAYING" }],
-  });
+    client.user.setPresence({
+        status: "dnd",
+        activities: [{name: `Ratio Bot ${process.env.MODE}`, type: "PLAYING"}],
+    });
 
-  const commands = [
-    new SlashCommandBuilder()
-      .setName("leaderboard")
-      .setDescription("Ratio Leaderboard")
-      .addBooleanOption((option) =>
-        option.setName("global").setDescription("Fetch global leaderboard")
-      ),
-  ].map((command) => command.toJSON());
+    const commands = [
+        new SlashCommandBuilder()
+            .setName("leaderboard")
+            .setDescription("Ratio Leaderboard")
+            .addBooleanOption((option) =>
+                option.setName("global").setDescription("Fetch global leaderboard")
+            ),
+    ].map((command) => command.toJSON());
 
-  await rest.put(
-    Routes.applicationGuildCommands(client.user.id, process.env.GUILD),
-    { body: commands }
-  );
-  await rest.put(Routes.applicationCommands(client.user.id), {
-    body: commands,
-  });
+    await rest.put(
+        Routes.applicationGuildCommands(client.user.id, process.env.GUILD),
+        {body: commands}
+    );
+    await rest.put(Routes.applicationCommands(client.user.id), {
+        body: commands,
+    });
 });
 
 client.on("messageCreate", async (message: Message) => {
-  if (message.author.bot || !message.content.toLowerCase().includes("ratio"))
-    return;
+    if (message.author.bot || message.content.match(/(?:^|\W)ratio(?:$|\W)/gmi).length === 0)
+        return;
 
-  const content = message.content.toLowerCase().split(" ");
+    try {
+        const emoji: string = (await topGG.hasVoted(message.author.id))
+            ? votedEmojis[
+                Math.floor(Math.random() * votedEmojis.length)
+                ]
+            : process.env.EMOJI;
 
-  if (content[0] !== "ratio" && content.pop() !== "ratio") return;
+        await message.react(emoji);
+        let msg: Message;
 
-  try {
-    //(maybe) random colored heart emoji if voted
-    const emoji: string = (await topGG.hasVoted(message.author.id))
-      ? process.env.VOTED_EMOJI
-      : process.env.EMOJI;
+        if (message.reference)
+            msg = await message.channel.messages.fetch(message.reference.messageId);
 
-    await message.react(emoji);
-    let msg: Message;
+        if (!msg) {
+            const msgs: Message[] = Array.from(
+                (await message.channel.messages.fetch()).values()
+            );
 
-    if (message.reference)
-      msg = await message.channel.messages.fetch(message.reference.messageId);
-
-    if (!msg) {
-      const msgs: Message[] = Array.from(
-        (await message.channel.messages.fetch()).values()
-      );
-
-      for (const m of msgs) {
-        if (
-          (message.mentions.users.size &&
-            message.mentions.users.first().id === m.author.id) ||
-          (!message.mentions.users.size && m.author.id !== message.author.id)
-        ) {
-          msg = m;
-          break;
+            for (const m of msgs) {
+                if (
+                    (message.mentions.users.size &&
+                        message.mentions.users.first().id === m.author.id) ||
+                    (!message.mentions.users.size && m.author.id !== message.author.id)
+                ) {
+                    msg = m;
+                    break;
+                }
+            }
         }
-      }
+        await msg.react(emoji);
+
+        await prisma.ratio.createMany({
+            data: [
+                {
+                    id: message.id,
+                    userId: message.author.id,
+                    serverId: message.guild.id,
+                    username: message.author.username,
+                    expire: Date.now() + 2592000000,
+                    related: msg.id,
+                },
+                {
+                    id: msg.id,
+                    userId: msg.author.id,
+                    serverId: msg.guild.id,
+                    username: msg.author.username,
+                    expire: Date.now() + 2592000000,
+                    related: message.id,
+                },
+            ],
+        });
+
+        if (cacheExpire > Date.now()) return;
+        client.user.setPresence({
+            status: "idle",
+            activities: [
+                {
+                    name: `${(await prisma.ratio.count()) / 2} ratios`,
+                    type: "WATCHING",
+                },
+            ],
+        });
+
+        // await prisma.ratio.deleteMany({
+        //   where: { expire: { lt: Date.now() } },
+        // });
+
+        cacheExpire = Date.now() + 60000;
+    } catch (e) {
+        if (process.env.mode === "DEV") console.log(e);
+        Sentry.captureException(e);
     }
-    await msg.react(emoji);
-
-    await prisma.ratio.createMany({
-      data: [
-        {
-          id: message.id,
-          userId: message.author.id,
-          serverId: message.guild.id,
-          username: message.author.username,
-          expire: Date.now() + 2592000000,
-          related: msg.id,
-        },
-        {
-          id: msg.id,
-          userId: msg.author.id,
-          serverId: msg.guild.id,
-          username: msg.author.username,
-          expire: Date.now() + 2592000000,
-          related: message.id,
-        },
-      ],
-    });
-
-    // const guildCheck: string[] = JSON.parse(
-    //   readFileSync("./servers.json").toString()
-    // );
-
-    // if (
-    //   !message.guild.me.permissions.has("USE_APPLICATION_COMMANDS") &&
-    //   !guildCheck.includes(message.guild.id)
-    // ) {
-    //   await message.channel.send({
-    //     embeds: [
-    //       new MessageEmbed()
-    //         .setTitle("Warning")
-    //         .setColor("DARK_RED")
-    //         .setDescription(
-    //           `Slash commands are disabled for the bot in this server! Without them, you cannot check ratio leaderboards. To enable slash commands, just reinvite the bot.`
-    //         )
-    //         .setFooter("This is a one time message"),
-    //     ],
-    //   });
-    //   guildCheck.push(message.guild.id);
-    //   writeFileSync("./servers.json", JSON.stringify(guildCheck));
-    // }
-
-    if (cacheExpire > Date.now()) return;
-    client.user.setPresence({
-      status: "idle",
-      activities: [
-        {
-          name: `${(await prisma.ratio.count()) / 2} ratios`,
-          type: "WATCHING",
-        },
-      ],
-    });
-
-    // await prisma.ratio.deleteMany({
-    //   where: { expire: { lt: Date.now() } },
-    // });
-
-    cacheExpire = Date.now() + 60000;
-  } catch (e) {
-    console.log(e);
-    webhookClient.send({
-      content: `THERE WAS AN ERROR!!!!!!!!!!!!!!!!!!!!`,
-      embeds: [
-        new MessageEmbed()
-          .setTitle("ERROR")
-          .setDescription(e.message)
-          .setColor("RED"),
-      ],
-    });
-  }
 });
 
 client.on("interactionCreate", async (interaction) => {
-  if (interaction.isCommand() && interaction.commandName === "leaderboard") {
-    await interaction.reply({
-      embeds: [
-        new MessageEmbed().setDescription(
-          "Loading <a:loading:921882048216576020"
-        ),
-      ],
-    });
+    if (interaction.isCommand() && interaction.commandName === "leaderboard") {
+        await interaction.reply({
+            embeds: [
+                new MessageEmbed()
+                    .setDescription("Loading <a:loading:921882048216576020>")
+                    .setColor("YELLOW"),
+            ],
+        });
 
-    const operation: any = {
-      orderBy: { likes: "desc" },
-      take: 10,
-    };
+        const operation: any = {
+            orderBy: {likes: "desc"},
+            take: 10,
+        };
 
-    if (!interaction.options.getBoolean("global") && interaction.inGuild())
-      operation.where = { serverId: interaction.guild.id };
-    const ratios = await prisma.ratio.findMany(operation);
+        if (!interaction.options.getBoolean("global") && interaction.inGuild())
+            operation.where = {serverId: interaction.guild.id};
+        const ratios = await prisma.ratio.findMany(operation);
 
-    let desc = "";
-    for (let i = 0; i < ratios.length; i++) {
-      const related = await prisma.ratio.findUnique({
-        where: { id: ratios[i].related },
-      });
-      desc += `${emojis[i]} ${ratios[i].username} ${process.env.EMOJI}**${ratios[i].likes}** ${md}≥${md} ${related.username} ${process.env.EMOJI}**${related.likes}**\n\n`;
+        let desc = "";
+        for (let i = 0; i < ratios.length; i++) {
+            const related = await prisma.ratio.findUnique({
+                where: {id: ratios[i].related},
+            });
+            desc += `${emojis[i]} ${ratios[i].username} ${process.env.EMOJI}**${ratios[i].likes}** ${md}≥${md} ${related.username} ${process.env.EMOJI}**${related.likes}**\n\n`;
+        }
+
+        await interaction.editReply({
+            embeds: [
+                new MessageEmbed()
+                    .setTitle("Ratio Leaderboard")
+                    .setColor("RANDOM")
+                    .setDescription(
+                        desc === "" ? "There are no ratios in this server." : desc
+                    )
+                    .setFooter(`Ratio Bot • Created by jonah#1234`),
+            ],
+        });
     }
-
-    await interaction.editReply({
-      embeds: [
-        new MessageEmbed()
-          .setTitle("Ratio Leaderboard")
-          .setColor("RANDOM")
-          .setDescription(
-            desc === "" ? "There are no ratios in this server." : desc
-          )
-          .setFooter(`Ratio Bot • Created by jonah#1234`),
-      ],
-    });
-  }
 });
 
 client.on("messageReactionAdd", (reaction) => manageReaction(reaction));
 client.on("messageReactionRemove", (reaction) => manageReaction(reaction));
 
 const manageReaction = async (
-  reaction: MessageReaction | PartialMessageReaction
+    reaction: MessageReaction | PartialMessageReaction
 ) => {
-  if (
-    [process.env.EMOJI, process.env.VOTED_EMOJI].includes(
-      `<:${reaction.emoji.name}:${reaction.emoji.id}>`
-    ) ||
-    reaction.count === 1
-  )
-    return;
+    try {
+        if (reaction.partial)
+            await reaction.fetch();
 
-  if (reaction.partial) reaction = await reaction.fetch();
+        if (
+            ![process.env.EMOJI, ...votedEmojis].includes(
+                `<:${reaction.emoji.name}:${reaction.emoji.id}>`
+            )
+        )
+            return;
 
-  await prisma.ratio.update({
-    where: { id: reaction.message.id },
-    data: { likes: reaction.count },
-  });
+
+        await prisma.ratio.update({
+            where: {id: reaction.message.id},
+            data: {likes: reaction.count},
+        });
+    } catch (e) {
+        if (process.env.mode === "DEV") console.log(e);
+        Sentry.captureException(e);
+    }
 };
 
 client.login(process.env.TOKEN);
